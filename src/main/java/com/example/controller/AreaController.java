7f45f2dd2efc62d1411c9a4113911b2221e09333
LeeNhung <93263032+LeeNhung@users.noreply.github.com>
2023-12-21 23:02:56 +0700

    Update AreaController.java

--- src/main/java/com/example/controller/AreaController.java
+++ src/main/java/com/example/controller/AreaController.java
@@ -20,7 +20,7 @@ import com.example.model.GeographicalArea;
 @RequestMapping("/area")
 public class AreaController {
 	private RestTemplate rest=new RestTemplate();
-	private String url="https://server-production-2465.up.railway.app";
+	private String url="https://da-server-production.up.railway.app";
 	@GetMapping
 	private String get(@RequestParam(name="key",defaultValue = "", required = false)String key,
 			HttpSession session, Model model) {
