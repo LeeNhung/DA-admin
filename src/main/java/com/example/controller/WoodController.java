a81c68e2fd68b615ab338a8e5001bd99e8db40f9
LeeNhung <93263032+LeeNhung@users.noreply.github.com>
2023-12-21 23:05:32 +0700

    Update WoodController.java

--- src/main/java/com/example/controller/WoodController.java
+++ src/main/java/com/example/controller/WoodController.java
@@ -40,7 +40,7 @@ import com.example.model.Wood;
 @RequestMapping("/wood")
 public class WoodController {
 	private RestTemplate rest=new RestTemplate();
-	private String url="https://server-production-2465.up.railway.app";
+	private String url="https://da-server-production.up.railway.app";
 	@GetMapping
 	private String get(Model model, HttpSession session , @RequestParam(name="category", defaultValue = "0", required = false)int category) {
 		List<Wood>listWood=Arrays.asList(rest.getForObject(url+"/wood/getAll?category="+category, Wood[].class));
