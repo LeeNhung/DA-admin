5731f3605350082110b92a6c80a39e7edd8601e6
LeeNhung <93263032+LeeNhung@users.noreply.github.com>
2023-12-21 23:04:35 +0700

    Update HomeController.java

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -22,7 +22,7 @@ import com.example.model.UserDTO;
 @RequestMapping("/")
 public class HomeController {
 	private RestTemplate rest=new RestTemplate();
-	private String url="https://server-production-2465.up.railway.app";
+	private String url="https://da-server-production.up.railway.app";
 	@GetMapping
 	private String getLogin(HttpSession session, HttpServletRequest request, Model model) {
 		User user=(User)session.getAttribute("user");
