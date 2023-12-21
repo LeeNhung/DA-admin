be0df77d88ac8d5970a6cde4428c6cee945a5eb7
LeeNhung <93263032+LeeNhung@users.noreply.github.com>
2023-12-21 22:31:58 +0700

    Update FamilyController.java

--- src/main/java/com/example/controller/FamilyController.java
+++ src/main/java/com/example/controller/FamilyController.java
@@ -23,7 +23,6 @@ import com.example.model.PlantFamily;
 public class FamilyController {
 	private RestTemplate rest=new RestTemplate();
 	private String url="https://server-production-2465.up.railway.app";
-	//private String url="https://da-server2-production.up.railway.app";
 	@GetMapping
 	private String getList(@RequestParam(name="key", defaultValue = "", required = false)String key, Model model, HttpSession session) {
 		System.out.println(url+"/plantfamily/get?key="+key);
